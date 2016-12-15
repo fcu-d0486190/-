a96f091a27682a6320c4b671c3058c90f19969c0
User <User@ASUS>
2016-12-16 02:15:58 +0800

    3

--- src/oop/Favorate.java
+++ src/oop/Favorate.java
@@ -1,25 +1,64 @@
 package oop;
-
 import java.util.Scanner;
-import java.util.*;
 
 public class Favorate {
 	Scanner scanner = new Scanner(System.in);
-	private String[] favorate = new String[5];
-	private int remind = 0;
+	private String[] favorate = {"null","null","null","null","null","null"};
+	private int remind = 0;//目前有幾個我的最愛
 	String choose, temp;
-	int pick;
+	int pick;//選擇我要刪的代碼
 	boolean loop = true;
-
+	
+	////////////////////////////////
+	private String[] coursecode = {"1","2","3","4","5","6"};//選課代碼
+	private String[] coursename = {"國防","計概","資料結構","微積分","戀愛心理學","物理"};
+	String coursenames;
+	public boolean getcoursecode(String code){
+		boolean found = false;
+		for(String coursecode : coursecode){
+			if(code.equals(coursecode)){
+				found = true;
+			}
+		}
+		return found;
+	}
+	public String getcoursename(String code){
+		if(code.equals("1")){
+			coursenames = getcoursename(0);
+		}
+		else if(code.equals("2")){
+			coursenames =  getcoursename(1);
+		}
+		else if(code.equals("3")){
+			coursenames = getcoursename(2);
+		}
+		else if(code.equals("4")){
+			coursenames = getcoursename(3);
+		}
+		else if(code.equals("5")){
+			coursenames =  getcoursename(4);
+		}
+		else if(code.equals("6")){
+			coursenames =  getcoursename(5);
+		}
+		return coursenames;
+	}
+	
+	public String getcoursename(int code){//傳課名稱
+		return coursename[code];
+	}
+	////////////////////////////
+	
+	
 	public void rewitreFavorate() {//修改我的最愛
 		while (loop) {
-			System.out.println("是否需要修改/n");
+			System.out.println("是否需要修改y/n");
 			choose = scanner.nextLine();
 			if (choose.equalsIgnoreCase("y")) {
 				display_favorate();
 				System.out.println("選擇想刪除號碼:");
 				pick = scanner.nextInt();
-				if (getremind() > pick && pick > 0) {
+				if (getremind() >= pick && pick > 0) {
 					for (int i = pick - 1; i < getremind(); i++) {
 						favorate[i] = favorate[i + 1];
 						setsubremind();
@@ -33,10 +72,11 @@ public class Favorate {
 				loop = false;
 			}
 		}
+		loop = true;
 	}
 
 	public boolean getFavorate(String coursuename) {//加我的最愛過程
-		if (remind != 5) {
+		if (remind < 5) {
 			for (int i = 0; i < remind; i++) {
 				if (!(coursuename.equals(favorate[i]))) {
 					if (remind == i) {
@@ -49,13 +89,17 @@ public class Favorate {
 					return false;
 				}
 			}
+			System.out.println(getremind());
+			favorate[getremind()] = coursuename;
+			setaddremind();
+			System.out.println(getremind());
 			System.out.println("成功加入我的最愛");
 			return true;
 		} else {
 			System.out.println("我的最愛已滿");
 			rewitreFavorate();
+			return false;
 		}
-		return false;
 	}
 
 	public void setaddremind() {// 控制上限
@@ -71,11 +115,11 @@ public class Favorate {
 	}
 
 	public void display_favorate() {// 印出我的最愛
+		int j = 1;
+		System.out.println(getremind());
 		for (int i = 0; i < getremind(); i++) {
-			int j = 1;
 			System.out.println(j + "." + favorate[i]);
 			j++;
 		}
 	}
-
 }
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -4,29 +4,66 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] argc) {
 		Scanner scanner = new Scanner(System.in);
-		Coursecode code = new Coursecode();
 		Favorate favorate = new Favorate();
-		String coursecode, choose;
-		String course = "國防";
-		boolean found;
-		coursecode = scanner.nextLine();//輸入選課代碼
-		if (code.getcoursecode(coursecode)) {
-			System.out.println("顯示課程資訊");//這裡要輸出相對應的課程
-			System.out.println("是否加入我的最愛y/n");//選則是否加入我的最愛
-			choose = scanner.nextLine();
-			if (choose.equalsIgnoreCase("y")) {//檢查是否有加入
-				if(favorate.getFavorate(course)){//有
-					favorate.setaddremind();//�最愛+1
+
+		while (true) {
+			System.out.println("輸入功能代碼");
+			int num = scanner.nextInt();
+			switch (num) {
+			case 1: {// 加入我的最愛
+				String coursecode, choose;
+				boolean found;
+				System.out.println("輸入選課代碼:");
+				coursecode = scanner.next();// 輸入選課代碼
+				if (favorate.getcoursecode(coursecode)) {
+					System.out.println("顯示課程資訊");// 這裡要輸出相對應的課程
+
+					System.out.println("1.我的最愛\n2.修改我的最愛\n3.無動作");
+					choose = scanner.next();
+					switch (choose) {
+					case "1": {
+						System.out.println("是否加入我的最愛y/n");// 選則是否加入我的最愛
+						choose = scanner.next();
+						if (choose.equalsIgnoreCase("y")) {// 檢查是否有加入
+							if (favorate.getFavorate(favorate.getcoursename(coursecode))) {// 有
+							}
+						} else {
+							System.out.println("無此代碼請重新輸入  http://查詢選課網址");
+						}
+						break;
+					}
+					case "2": {
+						favorate.display_favorate();
+						favorate.rewitreFavorate();
+						break;
+					}
+
+					case "3": {
+
+						break;
+					}
+					}
 				}
-				else{
-					
-				}
+				break;
 			}
-		} else {
-			System.out.println("無此代碼請重新輸入  http://查詢選課網址");
+			case 2: {
+				break;
+			}
+			case 3: {
+				break;
+			}
+			case 4: {
+				break;
+			}
+			case 5: {
+				break;
+			}
+			case 6: {
+				break;
+			}
+			}
 		}
 	}
 
